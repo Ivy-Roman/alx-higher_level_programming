5710601b974084b7c48365996146ac6076c5baa0
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-24 02:42:38 +0200

    fixed style error

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) < ord("a") or ord(c) > ord("z"):    
+    if ord(c) < ord("a") or ord(c) > ord("z"):
         return False
     else:
         return True
