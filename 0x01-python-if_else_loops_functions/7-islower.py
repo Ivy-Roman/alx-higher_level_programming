4a71e93d39c3df3f30847bb88b576d6abb279ede
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-24 02:40:21 +0200

    fixed output error

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) < chr(97) or ord(c) > chr(122):
+    if ord(c) < ord("a") or ord(c) > ord("z"):    
         return False
     else:
         return True
