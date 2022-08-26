c1edbb8c232acdd097ec09b889598ec1eaca0bd6
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-26 02:07:11 +0100

    fixed output error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     user_input = argv[1:]
     num_of_arg = len(user_input)
-    if num_of_arg == 0:
-        print("0 arguments.")
-    elif num_of_arg == 1:
-        print("1 argument:")
-        print("1: {:s}".format(argv[1]))
-    else:
-        print("{} arguments:".format(num_of_arg))
-        for index, arg in enumerate(user_input):
-            print("{:d}: {:s}".format(index + 1, argv[index]))
+    print("{:d} {:s}{:s}".
+          format(num_of_arg,
+                 "arguments" if (num_of_arg) != 1 else "argument",
+                 "." if (num_of_arg) == 0 else ":"))
+    for index, arg in enumerate(user_input):
+        print("{:d}: {:s}".format(index + 1, arg))
