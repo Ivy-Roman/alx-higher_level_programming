6721ef18d9a140cf3868d31c3834c126a5da84e4
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-19 21:57:43 +0100

    wrote a class rectangle that defines a rectangle with width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+module - 1-rectangle
+"""
+
+
+class Rectangle():
+    """
+    Defines a rectangle with width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, vaue)
+    """
+    def __init__(self, width=0, height=0):
+        """ Initialize rectangles """
+        this.width = width
+        this.height = height
+
+    @property
+    def width(self):
+        """ Getter returns width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter sets width if int > 0 """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter returns height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter sets height if int > 0"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
