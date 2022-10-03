328b6b7b270f0b33fd9dbe5cde80b8174ef8df0e
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-04 00:19:57 +0100

    wrote a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 100-my_int
+Contains class MyInt that inherits from int
+
+"""
+
+
+class MyInt(int):
+    """
+    Methods:
+        __init__(self, num)
+        __eq__(self, other)
+        __ne__(self, other)
+    """
+    def __init__(self, num):
+        """initialize num"""
+        self.num = num
+
+    def __notequal__(self, other):
+        """
+        Return:
+           True if both not equal
+        """
+        return self.num != other
+
+    def __equal__(self, other):
+        """
+        Return:
+           True if both equal
+        """
+        return self.num == other
