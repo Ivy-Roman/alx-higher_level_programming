8fdb552986d04ed728d861ace703bdb20e98e584
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-03 14:19:31 +0100

    fixed requirement error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -10,6 +10,12 @@ of the specified class; otherwise False
 
 def is_same_class(obj, a_class):
     """
-    returns True if the object is an instance of class, else false.
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+
+    Return:
+        True if the object is an instance of class, else false.
     """
     return type(obj) = a_class
