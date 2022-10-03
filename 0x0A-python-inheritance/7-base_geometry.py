dcc3d7f521d7d69c325b790ab42dfb07af6de200
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-03 21:46:47 +0100

    added a public instance method integer_validator that validates value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+module - 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    contains a method area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates input
+        Args:
+            name (str): assumed always a string
+            value (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
