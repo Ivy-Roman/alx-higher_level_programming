5b2edd43367ba0ab9f22507617fbc908e27a5496
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-03 21:30:03 +0100

    wrote added a method to the class that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module - 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    contains a method area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
