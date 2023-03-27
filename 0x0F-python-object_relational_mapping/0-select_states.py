b369c7a59f0719dc47866c9329a91327dc750f96
Ivie Osoiye <ivieosoiye@gmail.com>
2023-03-27 15:08:44 +0100

    corrected typos in variable names and added a skipped comma

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,13 +10,14 @@ from sys import argv
 if __name__ == "__main__":
 
     # connect to the database
-    db = MySQLdb.connect(
-            host=localhost,
-            port=3306
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            )
+    db = MySQLdb.connect( 
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+           
 
     # create cursor to execute queries
     # using SQL
@@ -24,5 +25,5 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
