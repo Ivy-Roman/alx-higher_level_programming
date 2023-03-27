cbdaf43b62adcd5bf2a9d32ddb1241573af1be2a
Ivie Osoiye <ivieosoiye@gmail.com>
2023-03-27 20:26:05 +0100

    wrote a script that lists the states matching the fourth argument from the database

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # create cursor to execute queries
+    # using SQL
+    cur = db.cursor()
+    sql_cmd = """SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    cur.execute(sql_cmd)
+    for row in cur.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
+
