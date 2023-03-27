7974236d88a4ff2d1561bd61d243612d2afafb8a
Ivie Osoiye <ivieosoiye@gmail.com>
2023-03-27 14:02:20 +0100

    wrote a script that prints all states from the given database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to the database
+    db = MySQLdb.connect(
+            host=localhost,
+            port=3306
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            )
+
+    # create cursor to execute queries
+    # using SQL
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
