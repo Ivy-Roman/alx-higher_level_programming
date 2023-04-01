fedfee2d4ed75b94dfa0b0ffeba87a2747602040
Ivie Osoiye <ivieosoiye@gmail.com>
2023-04-01 14:12:42 +0100

    wrote a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+return all cities objects from database
+parameters given to script: username, password, database
+"""
+
+from sys import argv
+from relationship_state import State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db}',
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query python instances in database
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
+
+    session.add(new_city)
+    session.add(new_state)
+    session.commit()
+
+    session.close()
