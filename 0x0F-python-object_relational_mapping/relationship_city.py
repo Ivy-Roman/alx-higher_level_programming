58fcc18ac93c887045ab5d57a49a67f33f41b43e
Ivie Osoiye <ivieosoiye@gmail.com>
2023-04-01 14:12:05 +0100

    improved the model_city and model_state to create them

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a state model
+Inherits from SQLAlchemy Base and links to the MySQL table cities
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Represents a city for a MySQL database.
+    __tablename__ (str): The name of the MySQL table to store cities.
+    id (sqlalchemy.Integer): The cities' id.
+    name (sqlalchemy.String): The cities' name.
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
